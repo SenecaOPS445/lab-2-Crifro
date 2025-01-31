20333f8800712a5193bc47b4c36f0289cc45044d
Cristian Fedor <cfedor@myseneca.ca>
2025-01-30 20:04:29 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Author: Cristian Fedor'''
+'''Author ID: 135196236'''
+'''Date Created: 2025/01/30'''
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
